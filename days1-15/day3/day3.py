311b49955eb8c931b0a1435407e676b6b32717fe
Amy Smith <amyjosm@gmail.com>
2021-07-13 22:09:29 -0700

    day 3 coding fin

--- days1-15/day3/day3.py
+++ days1-15/day3/day3.py
@@ -34,5 +34,28 @@ emailRegex = re.compile(r'''(
 # This will expression will catch most emails, but will not match EVERY possible email.
 
 # 3. Find all matches in clipboard text
+txt = str(pyperclip.paste())
+
+matches = []
+
+for groups in phoneRegex.findall(txt):
+    # Searching Phone #
+    # Contains str from groups 1, 3 & 5 -- area code, 3 digits, 4 digits
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    # Checking for group 8 and appending if found -- extension
+    if groups[8]!= '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+
+    # Searching Emails
+    # Appending the entire expression [0]
+    for groups in emailRegex.findall(txt):
+        matches.append(groups[0])
 
 # 4. Join matches into str for clipboard
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found.')
