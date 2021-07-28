075f75c8a528e7027ce8816328eb415333b47bfd
Amy Smith <amyjosm@gmail.com>
2021-07-28 10:04:24 -0700

    formatted with Black code style

--- days1-24/day2/day2.py
+++ days1-24/day2/day2.py
@@ -5,19 +5,19 @@
 import re
 
 # Creating regex object
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+phoneNumRegex = re.compile(r"\d\d\d-\d\d\d-\d\d\d\d")
 
 # Pass str you want to search using .search() method
-mo = phoneNumRegex.search('My number is 415-555-4242')
+mo = phoneNumRegex.search("My number is 415-555-4242")
 
-print(f'Phone number found: ', mo.group())
+print(f"Phone number found: ", mo.group())
 
 
-print('\n')
+print("\n")
 # Matching Multiple Groups with Pipe
-batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+batRegex = re.compile(r"Bat(man|mobile|copter|bat)")
 
-bruce = batRegex.search('Batmobile lost a wheel')
+bruce = batRegex.search("Batmobile lost a wheel")
 
 # Returns the entire word matched
 print(bruce.group())
@@ -26,93 +26,93 @@ print(bruce.group())
 print(bruce.group(1))
 
 
-print('\n')
+print("\n")
 # Optional Matching with Question Mark
-batRegex = re.compile(r'Bat(wo)?man')
+batRegex = re.compile(r"Bat(wo)?man")
 
-mo1 = batRegex.search('The Adventures of Batman')
+mo1 = batRegex.search("The Adventures of Batman")
 print(mo1.group())
 
-mo2 = batRegex.search('The Adventures of Batwoman')
+mo2 = batRegex.search("The Adventures of Batwoman")
 print(mo2.group())
 
 
-print('\n')
+print("\n")
 # Matching Specific Repetiions w/ Braces
 
 # This will match any str that has 'Ha' 3 or more times
-haRegex = re.compile(r'(Ha){3}')
+haRegex = re.compile(r"(Ha){3}")
 
 # Matches
-mo1 = haRegex.search('HaHaHa')
+mo1 = haRegex.search("HaHaHa")
 print(mo1.group())
 
 # Does not match
-mo2 = haRegex.search('Ha')
+mo2 = haRegex.search("Ha")
 print(mo2 == None)
 
 # Matches
-mo3 = haRegex.search('HaHaHaHaHa')
+mo3 = haRegex.search("HaHaHaHaHa")
 print(mo3.group())
 
 
-print('\n')
+print("\n")
 # FindAll() Method
 
 # search() returns the first matched text in the searched str
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+phoneNumRegex = re.compile(r"\d\d\d-\d\d\d-\d\d\d\d")
 
-mo = phoneNumRegex.search('Cell: 415-555-9999 Work: 212-555-0000')
-print(f'search() method', mo.group())
+mo = phoneNumRegex.search("Cell: 415-555-9999 Work: 212-555-0000")
+print(f"search() method", mo.group())
 
 # findall() returns EVERY match in the searched str
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+phoneNumRegex = re.compile(r"\d\d\d-\d\d\d-\d\d\d\d")
 
-mo2 = phoneNumRegex.findall('Cell: 415-555-9999 Work: 212-555-0000')
-print(f'findall() method', mo2)
+mo2 = phoneNumRegex.findall("Cell: 415-555-9999 Work: 212-555-0000")
+print(f"findall() method", mo2)
 
 
-print('\n')
+print("\n")
 # Making your own Character Class
 
 # Use [ ] to create custom character classes
 # This character class will match any vowel lowercase or uppercase
-vowelRegex = re.compile(r'[aeiouAEIOU]')
+vowelRegex = re.compile(r"[aeiouAEIOU]")
 
-mo = vowelRegex.findall('RoboCop eats baby food. BABY FOOD.')
+mo = vowelRegex.findall("RoboCop eats baby food. BABY FOOD.")
 print(mo)
 
 # Create a negative custom character class
 # This will match all characters NOT in the character class
-consonantRegex = re.compile(r'[^aeiouAEIOU]')
+consonantRegex = re.compile(r"[^aeiouAEIOU]")
 
-mo2 = consonantRegex.findall('RoboCop eats baby food. BABY FOOD.')
+mo2 = consonantRegex.findall("RoboCop eats baby food. BABY FOOD.")
 print(mo2)
 
 
-print('\n')
+print("\n")
 # Using the Caret Character
 
 # The caret symbol indicates a match at the beginning
-startHello = re.compile(r'^Hello')
+startHello = re.compile(r"^Hello")
 
-mo = startHello.search('Hello, world!')
+mo = startHello.search("Hello, world!")
 print(mo)
 
-mo2 = startHello.search('He said hello')
+mo2 = startHello.search("He said hello")
 print(mo2 == None)
 
 
-print('\n')
+print("\n")
 # Using the Dollar Sign Character
 
 # The dollar sign symbol indicates a match at the end
-endNum = re.compile(r'\d$')
+endNum = re.compile(r"\d$")
 
-mo = endNum.search('Your number is 42')
+mo = endNum.search("Your number is 42")
 print(mo)
 
-mo2 = endNum.search('Your number is forty-two')
+mo2 = endNum.search("Your number is forty-two")
 print(mo2 == None)
 
 
--- days1-24/day3/day3.py
+++ days1-24/day3/day3.py
@@ -5,7 +5,8 @@ import pyperclip
 import re
 
 # 1. Create a Regex for phone numbers
-phoneRegex = re.compile(r'''(
+phoneRegex = re.compile(
+    r"""(
     # Area Code -- ? means it is optional
     (\d{3}|\(\d{3}\))?
     # - or . seperator
@@ -18,10 +19,13 @@ phoneRegex = re.compile(r'''(
     (\d{4})
     # ext, x, or ext. for extension
     (\s*(ext|x|ext.)\s*(\d{2,5}))?
-    )''', re.VERBOSE)
+    )""",
+    re.VERBOSE,
+)
 
 # 2. Create a Regex for email addresses
-emailRegex = re.compile(r'''(
+emailRegex = re.compile(
+    r"""(
     # Username
     [a-zA-z0-9._%+-]+
     # @ symbol
@@ -30,7 +34,9 @@ emailRegex = re.compile(r'''(
     [a-zA-z0-9.-]+
     # .com, .org, .edu, etc
     (\.[a-zA-Z{2,4}])
-)''', re.VERBOSE)
+)""",
+    re.VERBOSE,
+)
 
 # This will expression will catch most emails, but will not match EVERY possible email.
 
@@ -42,10 +48,10 @@ matches = []
 for groups in phoneRegex.findall(txt):
     # Searching Phone #
     # Contains str from groups 1, 3 & 5 -- area code, 3 digits, 4 digits
-    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    phoneNum = "-".join([groups[1], groups[3], groups[5]])
     # Checking for group 8 and appending if found -- extension
-    if groups[8] != '':
-        phoneNum += ' x' + groups[8]
+    if groups[8] != "":
+        phoneNum += " x" + groups[8]
     matches.append(phoneNum)
 
     # Searching Emails
@@ -55,10 +61,10 @@ for groups in phoneRegex.findall(txt):
 
 # 4. Join matches into str for clipboard
 if len(matches) > 0:
-    pyperclip.copy('\n'.join(matches))
-    print('Copied to clipboard:')
-    print('\n'.join(matches))
+    pyperclip.copy("\n".join(matches))
+    print("Copied to clipboard:")
+    print("\n".join(matches))
 else:
-    print('No phone numbers or email addresses found.')
+    print("No phone numbers or email addresses found.")
 
 # Copied text from nostarch.com/contactus/
